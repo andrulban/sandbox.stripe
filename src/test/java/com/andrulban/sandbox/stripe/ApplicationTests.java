75ad0ec2bf3fce3033d9b77bc0a23d069210be9d
atytarchuk <tytarchuk.andrey@gmail.com>
2021-01-24 15:41:31 +0100

    init

---
+++ src/test/java/com/andrulban/sandbox/stripe/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrulban.sandbox.stripe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
